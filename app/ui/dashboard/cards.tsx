011cc2fbaa518bba741f5bd59ac70465b587c75a
jagath-sajjan <jagathsajjan097@gmail.com>
2025-09-20 16:21:05 +0530

    edit and deleting invocies

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -23,6 +23,7 @@ export default async function CardWrapper() {
   } = await fetchCardData();
   return (
     <>
+      {/* NOTE: Uncomment this code in Chapter 9 */}
 
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
@@ -62,4 +63,3 @@ export function Card({
     </div>
   );
 }
-
